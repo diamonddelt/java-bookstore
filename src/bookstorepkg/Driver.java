9854effb8f98a5786611b516ee1cad7c3a75d059
Ryan Rasti <diamonddelt@gmail.com>
2015-02-14 05:56:25 -0800

    Initial commit for Java project: Bookstore

---
+++ src/bookstorepkg/Driver.java
@@ -0,0 +1,11 @@
+package bookstorepkg;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// welcome user to the bookstore
+		Messages messages = new Messages();
+		messages.getGreeting();
+	}
+
+}
---
+++ src/bookstorepkg/Messages.java
@@ -0,0 +1,14 @@
+package bookstorepkg;
+
+/*
+ * Messages class provides useful messages that the bookstore
+ * needs to display based on various events
+ */
+public class Messages {
+
+	private String greeting = "Welcome to H&H Bookstore!";
+	
+	public String getGreeting() {
+		return greeting;
+	}
+}
