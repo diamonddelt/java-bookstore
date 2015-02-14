d92b0ba53a60144794b115f979c009cabdb0d446
Ryan Rasti <diamonddelt@gmail.com>
2015-02-14 15:21:12 -0800

    Added project to Eclipse workspace and I am testing out Java assertions

--- src/bookstorepkg/Driver.java
+++ src/bookstorepkg/Driver.java
@@ -1,11 +1,23 @@
 package bookstorepkg;
 
 public class Driver {
-
+	
+	private static boolean hasUserVisited = false;
+	
+	// NOTE: Assertions are enabled - disable them in Run Configurations -> remove "-ea" from VM config
 	public static void main(String[] args) {
+		
 		// welcome user to the bookstore
-		Messages messages = new Messages();
-		messages.getGreeting();
+		if (hasUserVisited != true)
+		{
+			// Testing assertions - assert that the hasUserVisited value is indeed true
+			assert (hasUserVisited == true);
+			
+			Messages messages = new Messages();
+			System.out.println(messages.getGreeting());
+			
+			hasUserVisited = true;
+		}
 	}
 
 }
